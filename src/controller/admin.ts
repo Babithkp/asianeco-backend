0e11d28b45e79c27bf27ccfa2a284d9feb87d2f8
babithkp <babithkullachetti44@gmail.com>
2025-06-28 09:04:07 +0530

    did some corrections

--- src/controller/admin.ts
+++ src/controller/admin.ts
@@ -3,36 +3,55 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
+export const createAdmin = async () => {
+  try {
+    const isAdminExists = await prisma.admin.findFirst();
+    if (!isAdminExists) {
+      await prisma.admin.create({
+        data: {
+          userName: "admin",
+          password: "admin@123",
+          purchaseValue: 0,
+        },
+      });
+    } else {
+      console.log("Admin already exists");
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
 
 export const adminLogin = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    if(!username || !password) {
-        res.status(400).json({
-            message: "Username and password are required",
-        })
-        return;
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).json({
+      message: "Username and password are required",
+    });
+    return;
+  }
+  try {
+    const user = await prisma.admin.findUnique({
+      where: {
+        userName: username,
+        password: password,
+      },
+    });
+    if (user) {
+      res.status(200).json({
+        message: "Login Successful",
+        user,
+      });
+    } else {
+      res.status(203).json({
+        message: "Invalid Credentials",
+      });
     }
-    try {
-        const user = await prisma.admin.findUnique({
-            where: {
-                userName: username,
-                password: password,
-            },
-        });
-        if (user) {
-            res.status(200).json({
-                message: "Login Successful",
-                user,
-            });
-        } else {
-            res.status(203).json({
-                message: "Invalid Credentials",
-            });
-        }
-    } catch (error) {
-        res.status(500).json({
-            message: "Internal Server Error",
-        });
-    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,38 @@ import express from "express";
 import cors from "cors";
 import adminRouter from "./router/admin";
 import clientRouter from "./router/client";
+import dotenv from "dotenv";
+import multer from "multer";
+import { createAdmin } from "./controller/admin";
 
 const app = express();
 
-// Middleware
+dotenv.config();
+
 app.use(cors());
-app.use(express.json()); // Important: This parses JSON request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+
+createAdmin();
+// Test routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
 
 // Routes
 app.use("/v1/admin", adminRouter);
+
 app.use("/v1/client", clientRouter);
 
-// Test routes
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
 
-app.get("/hello", (req, res) => {
-    res.send("Hello World!");
-});
+
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
